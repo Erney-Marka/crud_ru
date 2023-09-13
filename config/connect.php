ed26f88f669edb1c63a7f9948d081a8abc7b02e8
Erney-Marka <86185495+Erney-Marka@users.noreply.github.com>
2023-09-13 11:51:05 +0600

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('127.0.0.1', 'root', 'root', 'test_crud');  //соединение с базой данных
+$connect = mysqli_connect('127.0.0.1', 'root', 'root', 'test_crud');
 if(!$connect) {
     die('Error');
 }
